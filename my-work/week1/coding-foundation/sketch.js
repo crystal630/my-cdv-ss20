e5533f49503d05b1c848ed56181ffa3b1e3a543f
Yujia Liu <yl5804@nyu.edu>
2020-02-18 12:00:22 +0800

    Final version

--- my-work/week1/coding-foundation/sketch.js
+++ my-work/week1/coding-foundation/sketch.js
@@ -1,10 +1,16 @@
-
+var div=document.getElementById("1");
+console.log(div)
 function create(){
 num=document.getElementById("num").value
+div.innerHTML=""
 console.log(num)
 for(i=1;i<=num;i++){
   var btn=document.createElement("BUTTON")
-  document.body.appendChild(btn)
-  btn.setAttribute('id','rect')
+    btn.setAttribute('id','rect')
+    div.appendChild(btn)
+
+
 }
+
+
 }
