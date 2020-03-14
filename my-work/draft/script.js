0ed64197a3be8dd324cf2702584866e72d4e78ee
Yujia Liu <yl5804@nyu.edu>
2020-03-14 23:12:22 +0800

    draft

--- my-work/draft/script.js
+++ my-work/draft/script.js
@@ -7,14 +7,17 @@ function share(datapoint){
   return a;
 }
 function cordinateX(datapoint){
-  x=datapoint.time*200-100;
- return 'translate('+x+','+'150)'
+  x=datapoint.time*220-140;
+ return 'translate('+x+','+'100)'
 }
 
 function topss(datapoint){
   c=datapoint.tops;
   return c;
 
+}
+function stroke(d,i){
+   return d.sec;
 }
 function feet(datapoint){
   f=datapoint.feet
@@ -24,9 +27,14 @@ function pant(datapoint){
   p=datapoint.pants
   return p;
 }
+function coat(datapoint){
+  co=datapoint.coat;
+  return co;
+}
 
 var allGroup = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
-
+var x = d3.scaleLinear().range([0, 200]).domain([0, 50])
+var y = d3.scaleLinear().range([500, 200]).domain([0, 50]);
 function gotData(incomingData){
 
   // create svg
@@ -43,51 +51,69 @@ function gotData(incomingData){
       datagroup
       .append("rect")
       .attr("class", "tops")
-      .attr("x",20)
-      .attr("y",100)
-      .attr("width",180)
-      .attr("height",150)
-      .style("fill",topss)
+      .attr("x",0)
+      .attr("y",0)
+      .attr("width",130)
+      .attr("height",130)
+      .attr("fill",topss)
+      .attr("stroke-width",10)
+      .attr("stroke", stroke);
 
-    datagroup
-    .append("rect")
-    .attr("class","shoes")
-    .attr("x",30)
-    .attr("y",380)
-    .attr("width",180)
-    .attr("height",30)
-    .style("fill",feet);
-    datagroup
-    .append("circle")
-    .attr("class","head")
-    .attr("cx",60)
-    .attr("cy",0)
-    .attr("r",50)
-    .attr("fill","lightyellow")
-    datagroup
-    .append("rect")
+      datagroup
+      .append("rect")
+      .attr("class","shoes1")
+      .attr("x",-20)
+      .attr("y",420)
+      .attr("width",70)
+      .attr("height",30)
+      .style("fill",feet)
+      datagroup
+      .append("rect")
+      .attr("class","shoes2")
+      .attr("x",90)
+      .attr("y",420)
+      .attr("width",70)
+      .attr("height",30)
+      .style("fill",feet)
+      datagroup
+      .append("rect")
+      .attr("class","coat1")
+      .attr("x",0)
+      .attr("y",510)
+      .attr("width",130)
+      .attr("height",100)
+      .style("fill",coat)
+      datagroup
+      .append("rect")
+      .attr("class","coat2")
+      .attr("x",25)
+      .attr("y",500)
+      .attr("width",80)
+      .attr("height",130)
+      .style("fill",coat)
+
+
+var point = {"x": 4, "y": 31}
+
+var poly = [{"x":7, "y":50},
+        {"x":-3,"y":20},
+        {"x":10,"y":20},
+        {"x":17,"y":40},
+        {"x":24,"y":20},
+        {"x":37,"y":20},
+        {"x":27,"y":50},];
+
+    datagroup.selectAll(".pants").data([poly]).enter()
+    .append("polygon")
     .attr("class","pants")
-    .attr("x",55)
-    .attr("y",250)
-    .attr("width",120)
-    .attr("height",130)
-    .attr("fill",pant);
-    datagroup
-    .append("rect")
-    .attr("class","blank")
 
-     .attr("x",110)
-     .attr("y",290)
-     .attr("width",10)
-     .attr("height",150)
-     .style("fill","lightBlue")
-datagroup.append("g").selectAll("rect").data(getColors).enter()
-     .append("rect")
-     .attr("x", 30)
-     .attr("y", 420)
-     .attr("height", 30)
-     .attr("width",180)
-     .attr('fill',share);
+    .attr("points",function(d) {
+        return d.map(function(d) {
+            return [x(d.x),y(d.y)].join(",");
+        }).join(" ");
+    })
+    .attr("fill","lightyellow")//cant enter 2 datasets the same time
+
     datagroup.attr("transform", cordinateX);
 
 
